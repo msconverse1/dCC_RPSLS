61065b349095322bbecfd1f5ee78f393ec31927e
msconverse1 <msconverse@fullsail.edu>
2018-12-19 10:19:18 -0600

    Project Setup

---
+++ RPSLS/RPSLS/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class GameLogic
+    {
+        public List<string> Hand;
+        public GameLogic()
+        {
+            Hand = new List<string>() { "Rock", "Paper", "Scissors","Lizard","Spock" };
+            
+        }
+ 
+
+    }
+}
